cb0281e444827c7a09b48f1dd16748395ffcc8fc
Ayman250 <Aymanelmubark@gmail.com>
2016-01-15 23:54:17 -0600

    Still working on rising and falling...

---
+++ core/src/com/ZamanGames/RabbitGame/gameobjects/Weed.java
@@ -0,0 +1,11 @@
+package com.ZamanGames.RabbitGame.gameobjects;
+
+/**
+ * Created by Ayman on 1/15/2016.
+ */
+public class Weed extends Scrollable {
+
+    public Weed(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+    }
+}
